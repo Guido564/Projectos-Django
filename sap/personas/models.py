69f5a165c9c8aaa1d69b6b3c32fc6ddddeb8f551
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-25 12:09:53 -0300

    Manejo de la base de datos

--- sap/personas/models.py
+++ sap/personas/models.py
@@ -5,3 +5,6 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     edad = models.IntegerField()
+
+    def __str__(self):
+        return f'Persona {self.id} -- Nombre: {self.nombre} | Apellido: {self.apellido} | Edad: {self.edad}'
