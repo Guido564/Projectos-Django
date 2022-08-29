0e6eadaeafc19fa8a3663f9c71714f4448b0dce8
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-29 11:48:44 -0300

    Ordenar querys por ID

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -6,14 +6,14 @@ from personas.models import Persona, Domicilio
 
 # Create your views here.
 def bienvenido(request):
-    personas_total = Persona.objects.all()
+    personas_total = Persona.objects.order_by('id')
     no_personas = Persona.objects.count()
 
     return render(request, 'bienvenido.html', {'no_personas': no_personas, 'personas_total': personas_total})
 
 
 def domicilio(request):
-    domiclios_total = Domicilio.objects.all()
+    domicilios_total = Domicilio.objects.order_by('id')
     domicilios_nro = Domicilio.objects.count()
 
-    return render(request, 'domicilios.html', {'domicilios_nro': domicilios_nro, 'domiclios_total': domiclios_total})
+    return render(request, 'domicilios.html', {'domicilios_nro': domicilios_nro, 'domicilios_total': domicilios_total})
